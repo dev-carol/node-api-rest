f3e6a7574e13d26c31649c2d37d9ea6580e82cf6
carol santos <contato.caroline1999@gmail.com>
2021-10-26 00:04:14 -0300

    feat: added models to entitys

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,7 +4,7 @@
    
     app.post('/atendimentos', (req, res) => {
         console.log(req.body)
-   res.send ('Vocês está na rota de atendimentos e está realizando um post')
+   res.send ('Atendimento enviado')
  })
     
   }
